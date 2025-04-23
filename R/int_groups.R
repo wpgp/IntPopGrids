2f473153a153e26cdc725e9e52509c1b07dd0756
WB-zhang94 <52910815+WB-zhang94@users.noreply.github.com>
2025-04-23 15:21:29 +0100

    Update int_groups.R
    improved computation efficiency and friendly to small RAM

--- R/int_groups.R
+++ R/int_groups.R
@@ -24,75 +24,88 @@
 #' 
 #' @importFrom utils read.csv
 #' @export
-int_groups <- function(POP_path,group_path,admin_path,result_folder,method,workers) {
-  if (missing(POP_path)) {
-    stop("Parameter 'POP_path' is required and must be provided.")
-  }
-  if (missing(group_path)) {
-    stop("Parameter 'group_path' is required and must be provided.")
-  }
-  if (missing(admin_path)) {
-    stop("Parameter 'admin_path' is required and must be provided. If you only have one set of proportions for the whole raster, please provide a .tif file and assign a unique value to all non-NA pixels. The value should be the id value within the group file.")
-  }
-  if (missing(method)) {
-    method <- "random"
-  }
-  if (missing(result_folder)) {
-    result_folder <- getwd()
-  }
-  output_dir <- file.path(result_folder, "popint", sub("\\.[^.]*$", "", basename(POP_path)))
-  if (!dir.exists(output_dir)) {
-    dir.create(output_dir, recursive = TRUE)
-  }
-  temp_dir <- file.path(result_folder, "popint/tmp")
-  if (!dir.exists(temp_dir)) {
-    dir.create(temp_dir, recursive = TRUE)
-  }
+int_groups <- function(POP_path, group_path, admin_path,
+                       result_folder = getwd(), method = "random", workers = NULL) {
+  stopifnot(file.exists(POP_path),
+            file.exists(group_path),
+            file.exists(admin_path))
+  output_dir <- file.path(result_folder, "popint", tools::file_path_sans_ext(basename(POP_path)))
+  dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+  temp_dir <- file.path(result_folder, "popint", "tmp")
+  dir.create(temp_dir, recursive = TRUE, showWarnings = FALSE)
+  groups    <- read.csv(group_path)
+  admin_ids <- groups$id
+  if (is.null(workers)) workers <- max(1, availableCores() - 1)
+  plan(multisession, workers = workers)
   cat("* It may take some time to process.\n* In the meantime, it might be a good opportunity to relax and enjoy a cup of tea.\n")
-  groups <- read.csv(group_path)
-  if (missing(workers)) {
-    plan(multisession, workers = min(availableCores() - 1, nrow(groups)))
-  } else {
-    plan(multisession, workers = workers)
-  }
   cat("** Processing.....\n")
-  result_list <- future_lapply(groups$id, function(admin_id_i) {
-    POP_float <- rast(POP_path)
-    admin <- rast(admin_path)
-    admin_index <- admin == admin_id_i
-    pop_float_admin <- POP_float[admin_index]
-    pop_int_admin <- int_vector(pop_float_admin)
-    rm(POP_float, admin, admin_index, pop_float_admin)
-    gc()
-    group_i <- as.matrix(groups[groups$id == admin_id_i, !(names(groups) %in% "id")])
-    pop_int_admin_group <- disaggregate_int_vector(pop_int_admin,group_i,method)
-    rm(pop_int_admin, group_i)
-    gc()
-    temp_file <- file.path(temp_dir, paste0(admin_id_i,".rds"))
-    saveRDS(pop_int_admin_group, temp_file)
-    return(temp_file)
-  }, future.seed = NULL)
-  cat("** Writing int pop rasters in:\n**", output_dir)
-  if (missing(workers)) {
-    plan(multisession, workers = min(availableCores() - 1, ncol(groups) - 1))
-  } else {
-    plan(multisession, workers = min(workers))
-  }
-  future_lapply(1:(ncol(groups)-1), function(i) {
-    pop_rast <- rast(POP_path) * NA
-    admin <- rast(admin_path)
-    for (j in 1:nrow(groups)) {
-      pop_rast[admin == groups$id[j]] <- readRDS(result_list[[j]])[, i]
-    }
-    writeRaster(pop_rast,
-                file.path(output_dir, paste0(sub("\\.[^.]*$", "", basename(POP_path)),"_",names(groups)[i+1],".tif")),
-                gdal=c("COMPRESS=LZW", "BLOCKXSIZE=512", "BLOCKYSIZE=512", "TILED=YES", "BIGTIFF=YES"), 
-                overwrite=TRUE,
-                datatype="INT4U",
-                verbose=FALSE)
+  handlers("txtprogressbar")
+  result_list <- with_progress({
+    p <- progressor(along = admin_ids)
+    future_lapply(seq_along(admin_ids), function(i) {
+      admin_id <- admin_ids[i]
+      POP_float <- rast(POP_path)
+      admin <- rast(admin_path)
+      cell_idx <- terra::cells(admin, admin_id)[[1]]
+      pop_int_admin <- int_vector(as.matrix(POP_float[cell_idx]))
+      group_i       <- as.matrix(groups[i, -1])
+      pop_int_admin_group <- disaggregate_int_vector(pop_int_admin, group_i, method)
+      out_mat  <- cbind(matrix(cell_idx,ncol=1), pop_int_admin_group)
+      dimnames(out_mat)[[2]][1] <- "cell"
+      tmp_file <- file.path(temp_dir, paste0(admin_id, ".rds"))
+      saveRDS(out_mat, tmp_file, compress = TRUE)
+      p()
+    }, future.seed = TRUE)
   })
   plan(sequential)
+  cat("** Writing int pop rasters in:\n**", output_dir)
+  POP_float <- rast(POP_path)
+  admin     <- rast(admin_path)
+  ncell_tot  <- ncell(admin)
+  group_names <- names(groups)[-1]
+  ng <- length(group_names)
+  ff_path <- file.path(result_folder, "popint", "pop_matrix.ffdata")
+  if (file.exists(ff_path)) unlink(ff_path,force = T)
+  mat_ff  <- ff(vmode = "integer",
+                dim   = c(ncell_tot, ng),
+                filename = ff_path,
+                initdata = NA)
+  handlers("txtprogressbar")
+  with_progress({
+    p <- progressor(along = admin_ids)
+    for (id in admin_ids) {
+      dat <- readRDS(file.path(temp_dir, paste0(id, ".rds")))
+      mat_ff[dat[, 1], ] <- dat[, -1]
+      gc()
+      p()
+    }
+  })
+  close(mat_ff)
+  bs <- blocks(admin)
+  nc <- ncol(admin)
+  with_progress({
+    p <- progressor(steps = ng*bs$n)
+    for (g in seq_along(group_names)) {
+      out   <- rast(POP_path) * NA
+      fname <- file.path(output_dir,paste0(tools::file_path_sans_ext(basename(POP_path)),"_",group_names[g],".tif"))
+      writeStart(out, filename = fname,
+                 gdal = c("COMPRESS=LZW", "BLOCKXSIZE=512", "BLOCKYSIZE=512", "TILED=YES", "BIGTIFF=YES"),
+                 overwrite = TRUE,
+                 datatype = "INT4U",
+                 verbose = FALSE)
+      on.exit({writeStop(out);rm(out);gc()}, add = TRUE)
+      for (b in 1:bs$n) {
+        row0 <- bs$row[b]
+        nr   <- bs$nrows[b]
+        cell_idx <- ((row0 - 1L) * nc + 1L) : ((row0 + nr - 1L) * nc)
+        vals <- mat_ff[cell_idx, g]
+        writeValues(out, v = vals, start = bs$row[b], nrows = bs$nrows[b])
+        p()
+      }
+      writeStop(out)
+    } 
+  })
+  if (file.exists(ff_path)) unlink(ff_path,force = T)
   invisible()
+  cat("** Done!")
 }
-
-
