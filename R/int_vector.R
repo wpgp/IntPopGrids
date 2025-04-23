512886e7010547e24675b7318ec01461a26b609d
WB-zhang94 <52910815+WB-zhang94@users.noreply.github.com>
2025-04-23 15:22:52 +0100

    Update int_vector.R
    Fix a small bug when x_num = 0

--- R/int_vector.R
+++ R/int_vector.R
@@ -17,7 +17,7 @@ int_vector <- function(x) {
   x_dec <- x - x_int
   x_num <- sum(x_dec, na.rm=T)
   x_num <- round(x_num)
-  index <- order(x_dec, decreasing = T)[1:x_num]
+  index <- order(x_dec, decreasing = T)[0:x_num]
   x_int[index] <- x_int[index] + 1
   return(x_int)
 }
